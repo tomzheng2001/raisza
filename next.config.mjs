914d81fe6a28e0db206e8305e9740c9e0ca99dd5
Tom Zheng <tomzheng@Toms-MacBook-Air.local>
2024-06-17 02:25:07 -0700

    fixed next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 export const output = 'export';
-export const basePath = isProd ? '/<your-repo-name>' : '';
-export const assetPrefix = isProd ? '/<your-repo-name>/' : '';
+export const basePath = isProd ? '/raisza' : '';
+export const assetPrefix = isProd ? '/raisza/' : '';
 export const trailingSlash = true;
 export const images = {
     unoptimized: true, // Disable Image Optimization
